0e7fb136ff8ce13b16821df3e943704933e57b09
lcy958756276-dev <lcy958756276@gmail.com>
2026-04-10 18:26:36 +0800

    test push

--- Benchmark/vllm_server.py
+++ Benchmark/vllm_server.py
@@ -111,13 +111,13 @@ async def chat(request: Request):
     for words in user_stop_words:
         user_stop_tokens.append(tokenizer.encode(words))
     
-    # 构造prompt
- prompt_text, prompt_tokens = _build_prompt(
-    tokenizer,
-    query,
-    history=history,
-    system=system
-)
+        # 构造prompt
+    prompt_text, prompt_tokens = _build_prompt(
+        tokenizer,
+        query,
+        history=history,
+        system=system
+    )
         
     # vLLM请求配置
     sampling_params=SamplingParams(stop_token_ids=stop_words_ids, 
